9194595d02295b0ef8c22f6ef020739c5371347e
akayj <askingyj@gmail.com>
2023-06-16 19:28:27 +0800

    add autocmd

--- init.lua
+++ init.lua
@@ -5,3 +5,11 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+vim.api.nvim_create_autocmd({ "BufAdd", "BufEnter", "tabnew" }, {
+	callback = function()
+		vim.t.bufs = vim.tbl_filter(function(bufnr)
+			return vim.api.nvim_buf_get_option(bufnr, "modified")
+		end, vim.t.bufs)
+	end,
+})
